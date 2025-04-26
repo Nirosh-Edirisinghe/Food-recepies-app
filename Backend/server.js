4286f2dfc79666a5fc361cc8e00fab6195269b4b
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-04-26 16:30:43 -0700

    add packages

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv").config()
 const conncetDb = require("./config/connectionDb")
+const cors = require("cors")
 
 const  PORT = process.env.PORT || 3000
 conncetDb()
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/recipe",require("./routes/recipe"))
 
